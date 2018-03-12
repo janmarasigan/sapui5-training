5934b91becf398f09dd0541509ec3f1acd684a00
Andre Marasigan <janlmarasigan@gmail.com>
2018-03-12 23:04:53 +0800

    Modified component and controller to show transfer of jsonmodel

--- WebContent/hello-world/Component.js
+++ WebContent/hello-world/Component.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent"
+	"sap/ui/core/UIComponent"
 ], function(UIComponent) {
     "use strict";
     return UIComponent.extend("sapui5.training.helloworld.Component", {
@@ -11,3 +11,29 @@ sap.ui.define([
         }
     });
 });
+
+
+/* Solution to code exercise
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel"
+], function(UIComponent, JSONModel) {
+    "use strict";
+    return UIComponent.extend("sapui5.training.helloworld.Component", {
+        metadata : {
+            manifest : "json"
+        },
+        init : function() {
+            UIComponent.prototype.init.apply(this, arguments);
+            
+            var oData = {
+                recipient : {
+                    name : "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);  // <- this.setModel() is used instead of this.getView().setModel()
+        }
+    });
+});
+*/
