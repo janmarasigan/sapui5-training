0bc2cbdd915702318680a4ea2e16aa8ec4cd0cbb
Andre Marasigan <janlmarasigan@gmail.com>
2018-03-12 11:49:15 +0800

    Updated hello-world code

--- hello-world/Component.js
+++ hello-world/Component.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/UIComponent"
 ], function(UIComponent) {
     "use strict";
-    return UIComponent.extend("sapui5.training.template.Component", {
+    return UIComponent.extend("sapui5.training.helloworld.Component", {
         metadata : {
             manifest : "json"
         },
