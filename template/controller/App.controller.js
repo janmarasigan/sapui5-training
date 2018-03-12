fab4ae46d06b6b4131b1bc5bbc11ab2f7cbfa0ba
Andre Marasigan <janlmarasigan@gmail.com>
2018-03-12 22:08:38 +0800

    modified file structure

--- template/controller/App.controller.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
-    "use strict";
-    return Controller.extend("sapui5.training.template.controller.App", {
-
-    });
-});
