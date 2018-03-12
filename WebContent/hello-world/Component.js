8984dd1f3c92b6811db6e3f13208e2f9dfd172bd
Andre Marasigan <janlmarasigan@gmail.com>
2018-03-12 23:11:07 +0800

    Fix error in last commit on hello-world/Component.js

--- WebContent/hello-world/Component.js
+++ WebContent/hello-world/Component.js
@@ -26,13 +26,14 @@ sap.ui.define([
         init : function() {
             UIComponent.prototype.init.apply(this, arguments);
             
-//            var oData = {
-//                recipient : {
-//                    name : "World"
-//                }
-//            };
-//            var oModel = new JSONModel(oData);
-//            this.setModel(oModel);  // <- this.setModel() is used instead of this.getView().setModel()
+            var oData = {
+                recipient : {
+                    name : "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);  // <- this.setModel() is used instead of this.getView().setModel()
         }
     });
 });
+*/
