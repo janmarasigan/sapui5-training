748a714465ff7ac8175fb2a845167bb2bc4fb3ce
Andre Marasigan <janlmarasigan@gmail.com>
2018-03-13 08:17:35 +0800

    added button press event

--- WebContent/Chapter-3/controller/App.controller.js
+++ WebContent/Chapter-3/controller/App.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function(Controller, MessageToast) {
     "use strict";
     return Controller.extend("sapui5.training.template.controller.App", {
-
+    	onButtonPress : function() {
+    		MessageToast.show("I was pressed");
+    	}
     });
 });
